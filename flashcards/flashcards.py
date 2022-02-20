4cedc0a85cb49d3842856d79b2f7d431b09ffb43
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-02-20 15:39:53 +0100

    Stage 1/7: Stage one, card one

--- flashcards/flashcards.py
+++ flashcards/flashcards.py
@@ -1,1 +1,20 @@
-# Write your code here
+class Flashcard:
+    def __init__(self, term, definition):
+        self.term = term
+        self.definition = definition
+
+    def __repr__(self):
+        return f"Flashcard({self.term}, {self.definition})"
+
+    def __str__(self):
+        print_string = f"Card:\n{self.term}\nDefinition:\n{self.definition}"
+        return print_string
+
+
+def main():
+    card = Flashcard("purchase", "buy")
+    print(card)
+
+
+if __name__ == "__main__":
+    main()
