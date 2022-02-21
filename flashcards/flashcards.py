b1447e1473f304d8758ff67af90e5aecb9118919
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-02-21 11:12:36 +0100

    Stage 3/7: Make it your own

--- flashcards/flashcards.py
+++ flashcards/flashcards.py
@@ -1,6 +1,7 @@
-import re
-
 class Flashcard:
+    nb_flash_cards = 0
+    lst_flash_cards = []
+
     def __init__(self, term, definition):
         self.term = term
         self.definition = definition
@@ -13,12 +14,36 @@ class Flashcard:
         return print_string
 
 
+def set_of_cards():
+    print("Input the number of cards:")
+    Flashcard.nb_flash_cards = int(input())
+
+    for i in range(1, Flashcard.nb_flash_cards + 1):
+        print(f"The term for card #{i}:")
+        tmp_term = input()
+        print(f"The definition for card #{i}:")
+        tmp_definition = input()
+
+        tmp_card = Flashcard(tmp_term, tmp_definition)
+        tmp_card.lst_flash_cards.append(tmp_card)
+    return
+
+
+def check_set_of_cards():
+    for i in range(len(Flashcard.lst_flash_cards)):
+        curr_card = Flashcard.lst_flash_cards[i]
+        print(f'Print the definition of "{curr_card.term}":')
+        temp_definition = input()
+        if temp_definition == curr_card.definition:
+            print("Correct!")
+        else:
+            print(f'Wrong. The right answer is "f{curr_card.definition}".')
+    return
+
+
 def main():
-    term = input()
-    definition = input()
-    card = Flashcard(term, definition)
-    answer = input()
-    print("right" if re.match(card.definition, answer) else "wrong")
+    set_of_cards()
+    check_set_of_cards()
 
 
 if __name__ == "__main__":
